31def5b8bc39070b6f4567755f6f71c34fc917cc
Jon Horn <jhorn85@gmail.com>
2015-11-17 14:26:51 -0500

    Fucked up copy

--- models/index.js
+++ models/index.js
@@ -6,21 +6,41 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/wikistack'); // <= db name will be 'wikistack'
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongodb connection error:'));
+db.once('open', function (callback) {
+  // yay!
+  console.log("We are alive");
+});
 
 var statuses = ['open', 'closed'];
 
 var pageSchema = new mongoose.Schema({
-  title:    {type: String, required: true},
+  // title:    {type: String, required: true},
   urlTitle: {type: String, required: true},
-  content:  {type: String, required: true},
+  // content:  {type: String, required: true},
   status:   {type: String, enum: statuses},
   date:     {type: Date, default: Date.now},
   author:   {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
 });
 
 pageSchema.virtual('route').get(function() {
-  return "/wiki/" + this.route;
+  return "/wiki/" + this.urlTitle;
 });
+
+pageSchema.pre('validate', function(next) {
+  this.urlTitle = generateUrlTitle(this.title);
+  next();
+});
+
+function generateUrlTitle (title) {
+  if (title) {
+      // Removes all non-alphanumeric characters from title
+      // And make whitespace underscore
+      return title.replace(/\s+/g, '_').replace(/\W/g, '');
+  } else {
+      // Generates random 5 letter string
+      return Math.random().toString(36).substring(2, 7);
+  }
+}
 
 var userSchema = new mongoose.Schema({
   name: {type: String, required: true},
